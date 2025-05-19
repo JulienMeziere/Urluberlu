310476b94e79a33b0fefcacbc68880d4987094f6
Julien MEZIERE <julien@trackit.io>
2025-05-20 00:09:57 +0200

    fix css + deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -n "Enter password for remote server: "
+read -s PASSWORD
+echo ""
+
+sshpass -p "$PASSWORD" ssh julien@54.37.8.207 bash << EOF
+  cd ./Urluberlu &&
+  git pull &&
+  npm install &&
+  npm run build &&
+
+  # Deploy to the checklist subdomain's document root
+  echo "$PASSWORD" | sudo -S rm -rf /var/www/urluberlu/* &&
+  echo "$PASSWORD" | sudo -S cp -r dist/* /var/www/urluberlu/ &&
+  echo "$PASSWORD" | sudo -S chown -R www-data:www-data /var/www/urluberlu &&
+  echo "$PASSWORD" | sudo -S chmod -R 755 /var/www/urluberlu &&
+  echo "$PASSWORD" | sudo -S systemctl restart apache2
+EOF
