2512711d836078460ce039c06c39325ff1a42e62
Julien MEZIERE <julien@trackit.io>
2025-05-20 00:31:32 +0200

    fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,7 @@ sshpass -p "$PASSWORD" ssh julien@54.37.8.207 bash << EOF
   # Deploy to the checklist subdomain's document root
   echo "$PASSWORD" | sudo -S rm -rf /var/www/urluberlu/* &&
   echo "$PASSWORD" | sudo -S cp -r dist/* /var/www/urluberlu/ &&
+  echo "$PASSWORD" | sudo -S cp .htaccess /var/www/urluberlu/ &&
   echo "$PASSWORD" | sudo -S chown -R www-data:www-data /var/www/urluberlu &&
   echo "$PASSWORD" | sudo -S chmod -R 755 /var/www/urluberlu &&
   echo "$PASSWORD" | sudo -S systemctl restart apache2
